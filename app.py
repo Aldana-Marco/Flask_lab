bec4b42328a3777cbd38e2c4e156d12eea57d279
Marco Aldana <marco.aldana@3pillarglobal.com>
2021-10-19 15:38:24 -0500

    second commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 
 import services.player_services as player_service
+from repositories import player_repository
 
 app = Flask(__name__)
 
@@ -8,10 +9,17 @@ app = Flask(__name__)
 # endpoint
 
 @app.route('/users', methods=['GET', 'POST'])  # users and endpoints are in plural
-def hello_world():  # put application's code here
+def post_user():  # put application's code here
     body: dict = request.get_json(force=True)
-    user = player_service.create_player(body.get("name", "default"))
+    user = player_service.create_player(body.get("name", "default"))  # POST
     return jsonify(user.to_dict())
+
+
+@app.route('/users/list', methods=['GET'])
+def get_users():
+    for user in player_repository.PlayerRepository.users:
+        userdict = user.to_dict()
+        return jsonify(userdict)
 
 
 if __name__ == '__main__':
--- services/player_services.py
+++ services/player_services.py
@@ -7,5 +7,7 @@ player_repo = repository.PlayerRepository()
 def create_player(name: str):
     user = Player(len(player_repo.users) + 1, name, 0)
     player_repo.users.append(user)
-    print(len(player_repo.users))
     return user
+
+def get_players():
+    return player_repo.users
